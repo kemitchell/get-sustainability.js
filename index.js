2bd5cb6a8695233245e2b9f476888f36bbd9534e
Kyle E. Mitchell <kyle@kemitchell.com>
2019-04-14 09:14:37 -0700

    Simplify assertions

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ var schemas = {
 
 module.exports = function (options, callback) {
   // Validate arguments.
-  assert.strictEqual(typeof callback, 'function', 'callback argument must be a function')
-  assert.strictEqual(typeof options, 'object', 'options argument must be an object')
+  assert(typeof callback === 'function', 'callback argument must be a function')
+  assert(typeof options === 'object', 'options argument must be an object')
   var uri = options.uri
-  assert.strictEqual(typeof uri, 'string', 'uri option must be a string')
+  assert(typeof uri === 'string', 'uri option must be a string')
   var limit = options.limit || 5
-  assert.strictEqual(typeof limit, 'number', 'limit option must be a number')
+  assert(typeof limit === 'number', 'limit option must be a number')
   assert(Number.isInteger(limit), 'limit option must be an integer')
   assert(limit > 0, 'limit options must be greater than 0')
   var redirects = options.redirects || 1
-  assert.strictEqual(typeof redirects, 'number', 'redirects option must be a number')
+  assert(typeof redirects === 'number', 'redirects option must be a number')
   assert(Number.isInteger(redirects), 'redirects option must be an integer')
   assert(redirects > 0, 'redirects options must be greater than 0')
 
@@ -60,12 +60,12 @@ module.exports = function (options, callback) {
 }
 
 function get (options, callback) {
-  assert.strictEqual(typeof callback, 'function', 'callback argument must be a function')
-  assert.strictEqual(typeof options, 'object', 'options argument must be an object')
+  assert(typeof callback === 'function', 'callback argument must be a function')
+  assert(typeof options === 'object', 'options argument must be an object')
   var uri = options.uri
-  assert.strictEqual(typeof uri, 'string', 'uri option must be a string')
+  assert(typeof uri === 'string', 'uri option must be a string')
   var schemaName = options.schemaName
-  assert.strictEqual(typeof schemaName, 'string', 'schemaName option must be a string')
+  assert(typeof schemaName === 'string', 'schemaName option must be a string')
   assert(schemas.hasOwnProperty(schemaName), 'schemaName must be known schema')
   var redirects = options.redirects || 1
   assert(Number.isInteger(redirects), 'redirects option must be an integer')
